8dcaef1915ed586fb53520d61c53f56a8a21098a
Shalaka15299 <95271779+Shalaka15299@users.noreply.github.com>
2022-01-29 20:04:39 +0530

    Add files via upload

---
+++ view-pizza/view-pizza.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { AdminserviceService } from '../service/adminservice.service';
+import { IPizza } from 'src/app/pizza';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-view-pizza',
+  templateUrl: './view-pizza.component.html',
+  styleUrls: ['./view-pizza.component.css']
+})
+export class ViewPizzaComponent implements OnInit {
+  // avail: boolean = false;
+
+  public productList:any;
+  constructor(private adminService:AdminserviceService, private router:Router) { }
+
+  ngOnInit(): void {
+    this.adminService.getProducts()
+    .subscribe((res:any)=>{
+      this.productList=res;
+    })
+  }
+
+
+
+  deleteProduct(pizza:any){
+    var pizzaid=pizza.id;
+    this.adminService.deleteProduct(pizzaid)
+    .subscribe(data=>{
+      console.log(data);
+      this.adminService.avail=true;
+      this.adminService.message=data.resp;
+    })
+  }
+
+}
+
+
+
